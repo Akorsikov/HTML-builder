aa8d59e38ee5cc6a0f93d388c8980a32406b2812
Akorsikov <akors@mail.ru>
2022-05-20 19:28:08 +0300

    feat: 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const { stdin, stdout } = process;
+const pathMyFile = path.join(__dirname, 'mytext.txt');
+
+fs.writeFile( pathMyFile, '', (err) => {
+        if (err) throw err;
+    }
+);
+
+stdout.write('Введите текст (для завершения нажмите "Ctrl+C" или наберите exit):\n');
+stdin.on('data', data => {
+  const myBuffer = Buffer.from(data, 'utf-8').toString().trim();
+  if (myBuffer === 'exit') process.exit();
+  fs.appendFile(pathMyFile, data, err => {
+    if (err) throw err;
+  });
+});
+
+process.on('exit', () => stdout.write('Удачи в изучении Node.js!'));
